2b482b53236f1cd32ab85dfee002168f5fcdf584
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2026-01-19 23:56:35 +0530

    Implement addition of two complex numbers
    Addition Rule:
    (𝑎+b𝑖)+(𝑐+𝑑𝑖)=(𝑎+𝑐)+(𝑏+𝑑)𝑖
    (a+bi)+(c+di)=(a+c)+(b+d)i
    
    Logic Used
    Take real and imaginary parts from the user
    Add real parts together
    Add imaginary parts together
    Display the result

---
+++ ComplexNumberAddition.py
@@ -0,0 +1,10 @@
+r1 = int(input("Enter real part of first complex number: "))
+i1 = int(input("Enter imaginary part of first complex number: "))
+
+r2 = int(input("Enter real part of second complex number: "))
+i2 = int(input("Enter imaginary part of second complex number: "))
+
+real = r1 + r2
+imag = i1 + i2
+
+print("Sum =", real, "+", imag, "i")
