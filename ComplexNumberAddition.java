b24f94e064c7445ff7bc82a0fab8e9356542ba0a
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2026-01-20 00:07:19 +0530

    Implement complex number addition in Java
    Addition Rule:
    (𝑎+b𝑖)+(𝑐+𝑑𝑖)=(𝑎+𝑐)+(𝑏+𝑑)𝑖
    (a+bi)+(c+di)=(a+c)+(b+d)i
    
    Logic Used
    Take real and imaginary parts from the user
    Add real parts together
    Add imaginary parts together
    Display the result

---
+++ ComplexNumberAddition.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ComplexNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter real part of first number: ");
+        int r1 = sc.nextInt();
+        System.out.print("Enter imaginary part of first number: ");
+        int i1 = sc.nextInt();
+
+        System.out.print("Enter real part of second number: ");
+        int r2 = sc.nextInt();
+        System.out.print("Enter imaginary part of second number: ");
+        int i2 = sc.nextInt();
+
+        int real = r1 + r2;
+        int imag = i1 + i2;
+
+        System.out.println("Sum = " + real + " + " + imag + "i");
+    }
+}
